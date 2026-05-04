4480d7fc1a6023978b52a822110caefecd1e28a1
Michael Velez <steven.velez.2700@gmail.com>
2026-05-04 14:20:09 -0500

    feat(db): implement database connection

--- src/ReciclajeApp.Infrastructure/Persistence/AppDbContext.cs
+++ src/ReciclajeApp.Infrastructure/Persistence/AppDbContext.cs
@@ -2,8 +2,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ReciclajeApp.Infrastructure.Persistence;
 
-public sealed class AppDbContext : DbContext{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){
-        
+public sealed class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
     }
 }
